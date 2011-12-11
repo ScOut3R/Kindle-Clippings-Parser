22722c53bc73995338d122fa5b7cecb7cb4cf511
GABRI Mate <gabrimate@duosol.hu>
2011-12-11 12:08:29 +0100

    Simple DokuWiki output
    Parse My Clippings.txt and output highlights to [title].txt with
    unordered list containing the body and page (as footnote) of the
    highlight.
    Substituted spaces for tabs.

--- parser.py
+++ parser.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import re
 import codecs
 from pprint import pprint
@@ -5,42 +6,54 @@ from pprint import pprint
 EOR = u"=========="
 
 def records(file_path):
-    clip_file = codecs.open( file_path )
-    clip_file.seek( 3 ) # skip magic cookie
+	clip_file = codecs.open( file_path )
+	clip_file.seek( 3 ) # skip magic cookie
 
-    record = list()
-    for line in clip_file:
-        line = line.decode( 'utf-8' )
-        if line.strip() == EOR:
-            assert record[2] == '', "Non-blank line expected separating the header from the body of the clipping:%s" % record[2]
-            clip = dict()
-            match = re.match( r'(.*?)\((.*)\)', record[0] )
-            clip['title'], clip['attribution'] =match.groups() 
-            clip['attribution'] = clip['attribution'].split( ') (' )
+	record = list()
+	for line in clip_file:
+		line = line.decode( 'utf-8' )
+		if line.strip() == EOR:
+			assert record[2] == '', "Non-blank line expected separating the header from the body of the clipping:%s" % record[2]
+			clip = dict()
+			match = re.match( r'(.*?)\((.*)\)', record[0] )
+			clip['title'], clip['attribution'] =match.groups() 
+			clip['attribution'] = clip['attribution'].split( ') (' )
 
-            match = re.match( r'- (\w+) Loc. ([^|]+)\| Added on (\w+), (\w+ \d+, \d+), (\d+:\d+ \w\w)', record[1] )
-            clip['type'], clip['location'], clip['dow'], clip['date'], clip['time'] = match.groups()
+			match = re.match( r'- (\w+) on Page (\d+) \| Loc. ([^|]+)\| Added on (\w+), (\w+ \d+, \d+), (\d+:\d+ \w\w)', record[1] )
+			clip['type'], clip['page'], clip['location'], clip['dow'], clip['date'], clip['time'] = match.groups()
 
-            clip['body'] = "\n".join( record[3:] )
+			clip['body'] = "\n".join( record[3:] )
 
-            # a little tidying
-            clip['title'] = clip['title'].strip()
-            clip['location'] = clip['location'].strip()
+			# a little tidying
+			clip['title'] = clip['title'].strip()
+			clip['location'] = clip['location'].strip()
 
-            # yield and reset for next record
-            yield clip
-            record = list() 
-        else:
-            record.append( line.strip() )
+			# yield and reset for next record
+			if clip['type'] == 'Highlight':
+				yield clip
+			record = list() 
+		else:
+			record.append( line.strip() )
 
-    clip_file.close()
+	clip_file.close()
 
 
 
 if __name__ == '__main__':
-    from sys import argv
-    for n,r in enumerate( records(argv[1] ) ): #'My Clippings.txt') ):
-        print n, 
-        pprint( r )
+	from sys import argv
+	data = { }
+	for n,r in enumerate( records(argv[1] ) ): #'My Clippings.txt') ):
+		if data.has_key(r['title']):
+			data[r['title']].append(r)
+		else:
+			data[r['title']] = [ r ]
 
+	for title in data.keys():
+		output = open(title + '.txt', 'w')
+		string = ("====== %s ======\n\n" % title).encode('utf-8')
+		output.write(string)
+		for item in data[title]:
+			string = ("  * %s ((%s. p.))\n" % ( item['body'], item['page'] )).encode('utf-8')
+			output.write(string)
+		output.close()
 
